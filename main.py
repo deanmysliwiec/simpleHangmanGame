824a0901ec23208e6f173a5541a15bae5fe5d443
deanmysliwiec <32075153+deanmysliwiec@users.noreply.github.com>
2022-01-18 01:44:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -64,48 +64,63 @@ words = ['ant', 'baboon', 'badger', 'bat', 'bear', 'beaver', 'camel', 'cat', 'cl
 
 # highest it can go is 6 : print(HANGMANPICS[6])
 
+# choose random word
 comp_choice = random.choice(words)
+# remove it from word bank after it is used
 words.remove(comp_choice)
-# print(comp_choice)
+# For debugging: print(comp_choice)
 end = False
 correct = False
 won = False
+# reverse lives, if it hits 6, you lose
 lives = 0
+# holds the blanks and letters guessed if guessed right
 display = []
+# get length of word for blanks
 blanks = len(comp_choice)
+# add blanks to display, each representing "_"
 for _ in range(blanks):
     display += "_"
-
+# print word length for user
 print(f"Word length: {' '.join(display)}")
 
+# continuous until encounters break, could also use "end" variable present
 while not end:
     while not won and lives != 6:
-
+        # takes user input
         user_choice = input("What is your letter choice? ").lower()
+        # if already guessed letter
         if user_choice in display:
             print("\nYou already guessed that! But good try! \n")
+        # check for letters in word and add them to display if applicable
         for position in range(blanks):
             letter = comp_choice[position]
             if letter == user_choice:
                 correct = True
                 display[position] = letter
 
-
+        # if user guessed correctly on last guess
         if correct == True:
             print(f"You guessed right!\n {' '.join(display)} \n {HANGMANPICS[lives]}")
         else:
+            # if not guessed correctly, will (add) a life and add to hangman art
             lives += 1
             print(f"Incorrect!\n {' '.join(display)} \n {HANGMANPICS[lives]}")
+        # if no more blanks left, win game
         if "_" not in display:
             won = True
+        # correct set back to false for next guess
         correct = False
+        # if user has won
         if won == True:
             print("You won!!!")
+        # if not won, lives must be 6, otherwise it will continue through the loop again for another user guess
         elif lives == 6:
             print(f"You lost!!! The word was {comp_choice}")
-
+    # ask to play again
     choice = input("Would you like to play again? (Y/N): ").lower()
     if choice == "y":
+        # reset all variable data in order to fully reset game, still removing words from word bank
         lives = 0
         won = False
         display = []
